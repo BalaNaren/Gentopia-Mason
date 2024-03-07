e00a80dee84401d64e13883b02313217e0387c82
Bala Naren Chanumolu <4568naren@gmail.com>
2024-03-07 18:37:29 -0500

    Cloned vanilla and scholar agents

--- GentPool/gentpool/pool/__init__.py
+++ GentPool/gentpool/pool/__init__.py
@@ -9,3 +9,5 @@ from .vanilla_template import *
 from .openai_memory_template import *
 from .vanilla-ziyu import *
 from .scholar-ziyu import *
+from .cs678agent import *
+from .cs678scholaragent import *
